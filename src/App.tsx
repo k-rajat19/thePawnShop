225ecd26f4646600d8011042f3f7fbe3c424dd92
Rajat <robertdoweny2301@gmail.com>
2022-10-13 22:12:07 +0530

    CreateSingleAuction Window Done

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import AuctionHouse from './pages/MainMenu/AuctionHouse'
 import BuyPortal from './pages/MainMenu/BuyPortal'
 import ShopInterface from './pages/MainMenu/ShopInterface'
 import HottestAuction from './pages/MainMenu/HottestAuction';
+import CreateSingleAuction from './pages/MainMenu/CreateSingleAuction';
+import CreateCollectionAuction from './pages/MainMenu/CreateCollectionAuction';
 
 export default function App() {
 
@@ -21,6 +23,8 @@ export default function App() {
       <Route path="/shop_interface/Buy" element={<BuyPortal/>} />
       <Route path="/auction_house" element={<AuctionHouse/>}/>
       <Route path="/auction_house/hottest_auction" element={<HottestAuction/>}/>     
+      <Route path="/auction_house/create_single_auction" element={<CreateSingleAuction/>}/>     
+      <Route path="/auction_house/create_collection_auction" element={<CreateCollectionAuction/>}/>     
     </Routes>
     </Router> 
   )
--- src/pages/MainMenu/CreateSingleAuction/index.tsx
+++ src/pages/MainMenu/CreateSingleAuction/index.tsx
@@ -1,7 +1,115 @@
 import React from 'react'
+import Button from '../../../components/Button'
+import NavBar from '../../../components/NavBar'
+import TwistedBorder from '../../../components/TwistedBorder'
 
+const data=['','','','','','','','','','']
+const CreateAuctionBtnStyle={
+  height:'47px',
+  width:'100%',
+  backgroundColor:'#FFFFFF',
+  border:'none',
+  color:'#000000',
+  fontSize:'18px',
+  outline:'none',
+  cursor:'pointer',
+  display:'flex',
+  alignItems:'center',
+  justifyContent:'center'
+}
 export default function CreateSingleAuction() {
   return (
-    <div>CreateSingleAuction</div>
+    <div className="create-single-auction">
+       <NavBar chest={true}/>
+       <div className="create-single-auction-inner-wrapper">
+         
+         <div className="create-auction-machine">
+          <div className="inventory-section">
+            <div className="heading-container">
+              <span>Create single Auction</span>
+              <span>Please select item from inventory</span>
+            </div>
+              <TwistedBorder/>
+            <div className="title-container">
+              <span className='title'>My inventory</span>
+            </div>
+              <TwistedBorder/>
+            <div className="cards-container">
+              <div className="cards-container-inner-wrapper">
+                <div className="collections-wrapper">
+             {
+              data.map((value,index)=>{
+                return(
+
+                  
+                  <div className="collection" key={index} >
+                    <div className="image-container">
+
+                    </div>
+                  </div>
+                  
+                )
+                
+              })
+             }
+
+                </div>
+
+              </div>
+              
+            </div>
+          </div>
+          <VerticalBorder/>
+          <div className="create-section">
+             <div className="close-btn-container">
+             <img src="/close.png" alt="" />
+             </div>
+             <div className="connected-wallet-section">
+              <div className="heading">Connected wallet</div>
+              <div className="connected-wallet-container">
+                <div className="logo"><img src="" alt="" /></div>
+                <div className="info">
+                  <span>0008ssnxnxisks</span>
+                  <span>Metamask</span>
+                </div>
+              </div>
+             </div>
+            <form action="#">
+            
+              <label htmlFor="price">Start price</label>
+              <input type="text" id='price' placeholder='Enter Starting price' />
+            
+              <div className="time">
+                <div className='time-heading'>How long do you want this to run?</div>
+                <div className="time-container">
+                  <span className="time-btn active">12hrs</span>
+                  <span className="time-btn">24hrs</span>
+                  <span className="time-btn">48hrs</span>
+                  <input type="text" className='time-input' placeholder='Custom'/>
+                </div>
+            </div>
+              
+              <label htmlFor="name">Name</label>
+              <input type="text" id='name' placeholder='Name will be taken from NFt'/>
+              
+              
+              <label htmlFor="textarea">Description</label>
+              <textarea name="" id="textarea" placeholder='Please drop a brief description'/>
+              
+              <Button childern={'Create Auction'} styles={CreateAuctionBtnStyle}/>
+            </form>
+          </div>
+         </div>
+
+       </div>
+    </div>
+  )
+}
+
+function VerticalBorder(){
+  return(
+    <div className="vertical-border">
+
+    </div>
   )
 }
