e4ed2f8fd0c661058f9c1fbd39bd7592e7e586f9
Rajat <robertdoweny2301@gmail.com>
2022-10-13 22:44:38 +0530

    CreateCollectionAuction Design

--- src/pages/MainMenu/CreateCollectionAuction/index.tsx
+++ src/pages/MainMenu/CreateCollectionAuction/index.tsx
@@ -1,7 +1,100 @@
 import React from 'react'
+import Button from '../../../components/Button'
+import NavBar from '../../../components/NavBar'
+import TwistedBorder from '../../../components/TwistedBorder'
 
+const data=['','','','','','','','','','']
+const CreateAuctionBtnStyle={
+  height:'47px',
+  width:'100%',
+  backgroundColor:'#FFFFFF',
+  border:'none',
+  color:'#000000',
+  fontSize:'18px',
+  outline:'none',
+  cursor:'pointer',
+  display:'flex',
+  alignItems:'center',
+  justifyContent:'center'
+}
 export default function CreateCollectionAuction() {
   return (
-    <div></div>
+    <div className="create-collection-auction">
+       <NavBar chest={true}/>
+       <div className="create-collection-auction-inner-wrapper">
+         
+         <div className="create-auction-machine">
+          <div className="inventory-section">
+            <div className="heading-container">
+              <span>Create Collection Auction</span>
+              <span>Please upload picture below</span>
+            </div>
+              <TwistedBorder/>
+          <div className="upload-section">
+              <label htmlFor="upload">
+             <img src="/fadeplus.svg" alt="" />
+             <div className="text">
+              <span>Click to Upload file</span>
+              <span>PNG, GIF, WEBP, MP4 or MP3.<br/> Max 100mb.</span>
+             </div>
+             
+
+
+              </label>
+            <input type="file" id='upload'/>
+          </div>
+          </div>
+          <VerticalBorder/>
+          <div className="create-section">
+             <div className="close-btn-container">
+             <img src="/close.png" alt="" />
+             </div>
+             <div className="connected-wallet-section">
+              <div className="heading">Connected wallet</div>
+              <div className="connected-wallet-container">
+                <div className="logo"><img src="" alt="" /></div>
+                <div className="info">
+                  <span>0008ssnxnxisks</span>
+                  <span>Metamask</span>
+                </div>
+              </div>
+             </div>
+            <form action="#">
+            
+              <label htmlFor="price">Start price</label>
+              <input type="text" id='price' placeholder='Enter Starting price' />
+            
+              <div className="time">
+                <div className='time-heading'>How long do you want this to run?</div>
+                <div className="time-container">
+                  <span className="time-btn active">12hrs</span>
+                  <span className="time-btn">24hrs</span>
+                  <span className="time-btn">48hrs</span>
+                  <input type="text" className='time-input' placeholder='Custom'/>
+                </div>
+            </div>
+              
+              <label htmlFor="name">Name</label>
+              <input type="text" id='name' placeholder='Name will be taken from NFt'/>
+              
+              
+              <label htmlFor="textarea">Description</label>
+              <textarea name="" id="textarea" placeholder='Please drop a brief description'/>
+              
+              <Button childern={'Create Auction'} styles={CreateAuctionBtnStyle}/>
+            </form>
+          </div>
+         </div>
+
+       </div>
+    </div>
+  )
+}
+
+function VerticalBorder(){
+  return(
+    <div className="vertical-border">
+
+    </div>
   )
 }
