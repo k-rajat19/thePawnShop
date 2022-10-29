b2e8795171ed4f1e72f3129b60c272fa6760a10f
Rajat <robertdoweny2301@gmail.com>
2022-10-30 00:16:22 +0530

    setup: ESLint Prettier Husky

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    semi: 0,
+    'comma-dangle': 0,
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+  },
+}
